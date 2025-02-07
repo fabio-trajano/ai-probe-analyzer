b1a2e11063bacd2dce5b79a88a60c1ee72e4aee4
Fabio Trajano Ramalho <114175707+fabio-trajano@users.noreply.github.com>
2025-02-07 20:06:25 +0000

    Initial commit

---
+++ src/main/java/com/portfolio/ai_probe_analyzer/AiProbeAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ai_probe_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProbeAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProbeAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/ai_probe_analyzer/AiProbeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ai_probe_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProbeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
