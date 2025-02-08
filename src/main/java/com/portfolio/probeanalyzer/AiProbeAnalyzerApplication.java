d46c8a1750dbf6ed4978544d57c845a58504f637
Fabio Trajano Ramalho <114175707+fabio-trajano@users.noreply.github.com>
2025-02-08 10:56:33 +0000

    properties and application

--- src/main/java/com/portfolio/probeanalyzer/AiProbeAnalyzerApplication.java
+++ src/main/java/com/portfolio/probeanalyzer/AiProbeAnalyzerApplication.java
@@ -1,9 +1,11 @@
-package com.portfolio.ai_probe_analyzer;
+package com.portfolio.probeanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AiProbeAnalyzerApplication {
 
 	public static void main(String[] args) {
